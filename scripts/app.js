2a9f89617a64c49ea436e7db5965a21c1bc7c6c2
cptiwari20 <cptiwari20@gmail.com>
2018-11-27 17:56:06 +0530

    added the basic form

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,62 @@
-'use strict';
+"use strict";
 
-var templete = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Indecision App'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'This is an indecision app'
-  )
-);
 var appRoot = document.querySelector('#app');
 
-ReactDOM.render(templete, appRoot);
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your options inside your mind into computer",
+  options: ["one", "two"]
+};
+
+function addOption(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  if (!option) {
+    return console.log('Please enter some option!');
+  }
+  app.options.push(option);
+  e.target.elements.option.value = "";
+  renderDOM();
+}
+
+function renderDOM() {
+
+  var templete = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      "ul",
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          "li",
+          { key: option },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: addOption },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        { type: "submit" },
+        "Add option"
+      )
+    )
+  );
+
+  ReactDOM.render(templete, appRoot);
+}
+renderDOM();
--- src/app.js
+++ src/app.js
@@ -1,9 +1,41 @@
-var templete = (
-  <div>
-    <h1>Indecision App</h1>
-    <p>This is an indecision app</p>
-  </div>
-)
-var appRoot = document.querySelector('#app');
+const appRoot = document.querySelector('#app');
 
-ReactDOM.render(templete, appRoot)
+const app = {
+  title: "Indecision App",
+  subtitle: "Put your options inside your mind into computer",
+  options: ["one", "two"]
+}
+
+function addOption(e){
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if(!option){
+    return console.log('Please enter some option!')
+  }
+  app.options.push(option);
+  e.target.elements.option.value = "";
+  renderDOM();
+}
+
+function renderDOM(){
+
+    const templete = (
+      <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ul>
+          {app.options.map(option => {
+            return <li key={option}>{option}</li>
+          })}
+        </ul>
+
+        <form onSubmit={addOption}>
+          <input type="text" name="option"/>
+          <button type='submit'>Add option</button>
+        </form>
+      </div>
+    )
+
+    ReactDOM.render(templete, appRoot)
+}
+renderDOM()
