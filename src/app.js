4bbc4d6073ff40458e56ac2137234642944a376b
cptiwari20 <cptiwari20@gmail.com>
2018-11-28 18:02:25 +0530

    added    babel-plugin for class-properties

--- src/app.js
+++ src/app.js
@@ -4,3 +4,27 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/Indecision-App';
 
 ReactDOM.render(<IndecisionApp />, document.querySelector('#app'));
+
+class A{
+  constructor(){
+    this.name = 'vikas';
+    this.getName = this.getName.bind(this);
+  }
+
+  getName(){
+    return(`My name is ${this.name}`)
+  } 
+};
+const a = new A();
+console.log(a.getName());
+
+class Me{
+  name= 'Cp Tiwari';
+
+  getMyName = () => {
+    return(`My name is ${this.name}`)
+  }
+}
+const i = new Me();
+const nam = i.getMyName()
+console.log(nam)
