d0e9e6c15934db77da74f52b12d8a51a79e64a63
cptiwari20 <cptiwari20@gmail.com>
2018-11-28 21:40:46 +0530

    added error hadler to the addOption form

--- src/components/AddOptions.js
+++ src/components/AddOptions.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
 export default class AddOptions extends Component {
+  state = { error: undefined }
   handleAdd = (e) => {
     e.preventDefault();
     const option = e.target.elements.option.value;
     if(!option){
-      return alert("ERROR: Please enter some text")
+      return this.setState({ error: "ERROR: Please enter some text"})
     }
-    this.props.handleAddOption(option)
+    this.props.handleAddOption(option);
     e.target.elements.option.value = "";
+    this.setState({error: undefined});
   }
   render(){
     return(
       <form onSubmit={this.handleAdd}>
+          {this.state.error && <h4>{this.state.error}</h4>}
           <input type="text" name="option"/>
           <button type='submit'>Add more options</button>
       </form>
