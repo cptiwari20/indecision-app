e3064de1ad82b0fd2a8a953910c5b600a0c9dc52
cptiwari20 <cptiwari20@gmail.com>
2018-11-29 18:07:55 +0530

    added scss files for styling header

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/Indecision-App';
-import './style.scss';
+import './styles/style.scss';
 
 ReactDOM.render(<IndecisionApp />, document.querySelector('#app'));
